c7493bf391148ae4dfac16563171026ce827eec9
hannah <hannah.l.curran@gmail.com>
2023-07-05 19:50:24 +0100

    labs 2 will be complete

--- models/skill.js
+++ models/skill.js
@@ -1,19 +1,33 @@
 const skills = [
-{id: 176534, skill: 'Technical Knowledge', achieved: false},
-{id: 198723, skill: 'Problem Solving', achieved: true},
-{id: 109459, skill: 'Curious Attitude', achieved: true}
+    { id: 176534, skill: 'Technical Knowledge', achieved: false },
+    { id: 198723, skill: 'Problem Solving', achieved: true },
+    { id: 109459, skill: 'Curious Attitude', achieved: true }
 ];
 
 module.exports = {
     getAll,
-    getOne 
+    getOne,
+    create,
+    deleteOne
 };
 
-function getOne(id){ 
+function deleteOne(id) {
     id = parseInt(id);
-    return skills.find(skill=>skill.id === id)
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
 }
 
-function getAll(){
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.achieved = false;
+    skills.push(skill);
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skill.id === id);
+}
+
+function getAll() {
     return skills;
 }
